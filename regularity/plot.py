d1d3eaa4e0e0da3dea3c924d973894ee4f76e853
ririgi <lrpbaltazar@gmail.com>
2019-09-12 15:38:49 +0800

    add plotting of tenure

--- regularity/plot.py
+++ regularity/plot.py
@@ -21,9 +21,28 @@ sns.set()
 style.use('seaborn-poster')
 style.use('bmh')
 
-file = "results/regularity.csv"
-df = readChunk(file)
-df['RWEEK'] = df['RWEEK'].astype(int)
+def plotRegularityFreq():
+	file = "results/feb3_regularity.csv"
+	df = readChunk(file, header = None)
+	df.rename(columns = {0:'WEEK', 8:'RWEEK', 9:'USERID'}, inplace = True)
+	df['RWEEK'] = df['RWEEK'].astype(int)
+	plot = sns.distplot(a = df['RWEEK'].values, kde = False, bins = 7)
+	new_df = df.groupby('USERID')['RWEEK'].mean().to_frame()
+	new_df['RWEEK'] = round(new_df['RWEEK'])
 
-plot = sns.distplot(a = df['RWEEK'].values, kde = False, bins = 7)
-plt.show()
+	plot = sns.distplot(a = new_df['RWEEK'].values, kde = False, bins = 7)
+	plt.show()
+
+def plotRegularityTenure():
+	file = 'results/tenure.csv'
+	df = readChunk(file)
+	df['RWEEK'] = df['RWEEK'].astype(float)
+	df['TENURE'] = df['TENURE'].astype(float)
+
+	for i in df.RWEEK.unique():
+		temp = df.loc[df.RWEEK == i]
+		plot = sns.distplot(a = temp['TENURE'].values, kde = False)
+		plt.show()
+
+if __name__ == '__main__':
+	plotRegularityTenure()
