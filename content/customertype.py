9ca68a7142a9662f1e6044a18aa849d45ab7b2b7
ririgi <lrpbaltazar@gmail.com>
2019-09-24 16:07:17 +0800

    add getting of customer type based on content watched

--- content/customertype.py
+++ content/customertype.py
@@ -26,4 +26,21 @@ origshow = pd.read_csv('results/origshow.csv')
 origshow['ORIGSHOW'] = 1
 label = origshow[['USERID', 'ORIGSHOW']].merge(label, how = 'right', on = 'USERID')
 
-print(label.head())
+label['sum'] = label['CHANNEL2'] + label['MOVIE'] + label['ORIGSHOW'] + label['ORIGMOVIE']
+
+def getCustomerType(row):
+	if row['sum'] == 4: return "DIVERSE"
+	elif row['sum'] == 3: return "CHANNEL2_ORIGINAL"
+	elif row['sum'] == 2:
+		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == np.nan) & (row['ORIGMOVIE'] == np.nan): return "CHANNEL2_MOVIE"
+		else: return "ORIGINALS"
+	elif row['sum'] == 1:
+		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == np.nan) & (row['ORIGSHOW'] == np.nan) & (row['ORIGMOVIE'] == np.nan): return "CHANNEL2"
+		elif (row['MOVIE'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == np.nan) & (row['ORIGMOVIE'] == np.nan): return "MOVIE"
+
+contenttype = []
+for i in range(len(label)):
+	contenttype.append(label.iloc[i])
+
+label['CONTENT_TYPE_WATCHED'] = contenttype
+label.to_csv('results/customer_feature_matrix_v2.csv')
