bc6967bc8cb5897838a06748d0d4a26ae8121150
ririgi <lrpbaltazar@gmail.com>
2019-09-24 15:50:28 +0800

    add label

--- content/customertype.py
+++ content/customertype.py
@@ -14,12 +14,16 @@ label.columns = label.columns.str.upper()
 
 label = label.loc[label.LABEL == 'ACTIVE']
 channel2 = pd.read_csv('results/channel2.csv')
-label = channel2.merge(label, how = 'right', on = 'USERID')
+channel2['CHANNEL2'] = 1
+label = channel2[['USERID', 'CHANNEL2']].merge(label, how = 'right', on = 'USERID')
 movie = pd.read_csv('results/movie.csv')
-label = movie.merge(label, how = 'right', on = 'USERID')
+movie['MOVIE'] = 1
+label = movie[['MOVIE', 'MOVIE']].merge(label, how = 'right', on = 'USERID')
 origmovie = pd.read_csv('results/origmovie.csv')
-label = origmovie.merge(dflabel2, how = 'right', on = 'USERID')
+origmovie['ORIGMOVIE'] = 1
+label = origmovie[['USERID', 'ORIGMOVIE']].merge(label, how = 'right', on = 'USERID')
 origshow = pd.read_csv('results/origshow.csv')
-label = origshow.merge(label, how = 'right', on = 'USERID')
+origshow['ORIGSHOW'] = 1
+label = origshow[['USERID', 'ORIGSHOW']].merge(label, how = 'right', on = 'USERID')
 
 print(label.head())
