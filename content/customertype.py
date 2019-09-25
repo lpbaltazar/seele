b7b13cee4627da6567d09202229a225ec5813ec0
ririgi <lrpbaltazar@gmail.com>
2019-09-25 09:16:55 +0800

    toedit in mac

--- content/customertype.py
+++ content/customertype.py
@@ -9,54 +9,59 @@ import time
 import pandas as pd
 import numpy as np
 
-label = pd.read_csv("../data/customer_feature_matrix.csv", usecols = ["userid", "label"])
-label.columns = label.columns.str.upper()
+# label = pd.read_csv("../data/customer_feature_matrix.csv", usecols = ["userid", "label"])
+# label.columns = label.columns.str.upper()
 
-print('combining data')
-label = label.loc[label.LABEL == 'ACTIVE']
-channel2 = pd.read_csv('results/channel2.csv')
-channel2['CHANNEL2'] = 1
-label = channel2[['USERID', 'CHANNEL2']].merge(label, how = 'right', on = 'USERID')
-movie = pd.read_csv('results/movie.csv')
-movie['MOVIE'] = 1
-label = movie[['USERID', 'MOVIE']].merge(label, how = 'right', on = 'USERID')
-origmovie = pd.read_csv('results/origmovie.csv')
-origmovie['ORIGMOVIE'] = 1
-label = origmovie[['USERID', 'ORIGMOVIE']].merge(label, how = 'right', on = 'USERID')
-origshow = pd.read_csv('results/origshow.csv')
-origshow['ORIGSHOW'] = 1
-label = origshow[['USERID', 'ORIGSHOW']].merge(label, how = 'right', on = 'USERID')
-label.fillna(0, inplace = True)
-print(label.head())
-label['sum'] = label['CHANNEL2'] + label['MOVIE'] + label['ORIGSHOW'] + label['ORIGMOVIE']
+# print('combining data')
+# label = label.loc[label.LABEL == 'ACTIVE']
+# channel2 = pd.read_csv('results/channel2.csv')
+# channel2['CHANNEL2'] = 1
+# label = channel2[['USERID', 'CHANNEL2']].merge(label, how = 'right', on = 'USERID')
+# movie = pd.read_csv('results/movie.csv')
+# movie['MOVIE'] = 1
+# label = movie[['USERID', 'MOVIE']].merge(label, how = 'right', on = 'USERID')
+# origmovie = pd.read_csv('results/origmovie.csv')
+# origmovie['ORIGMOVIE'] = 1
+# label = origmovie[['USERID', 'ORIGMOVIE']].merge(label, how = 'right', on = 'USERID')
+# origshow = pd.read_csv('results/origshow.csv')
+# origshow['ORIGSHOW'] = 1
+# label = origshow[['USERID', 'ORIGSHOW']].merge(label, how = 'right', on = 'USERID')
+# label.fillna(0, inplace = True)
+# print(label.head())
+# label['sum'] = label['CHANNEL2'] + label['MOVIE'] + label['ORIGSHOW'] + label['ORIGMOVIE']
 
-def getCustomerType(row):
-	if row['sum'] == 4: return "DIVERSE"
-	elif row['sum'] == 3:
-		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 1): return "CHANNEL2_ORIGINAL"
-		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "DIVERSE"
-		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "DIVERSE"
-		elif (row['CHANNEL2'] == 0) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 1): return "ORIGINAL_MOVIES"
-	elif row['sum'] == 2:
-		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 0): return "CHANNEL2_MOVIE"
-		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "CHANNEL2_ORIGINAL"
-		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "CHANNEL2_ORIGINAL"
-		elif (row['CHANNEL2'] == 0) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "ORIGINAL_MOVIES"
-		elif (row['CHANNEL2'] == 0) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "ORIGINAL_MOVIES"
-		else: return "ORIGINALS"
-	elif row['sum'] == 1:
-		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 0): return "CHANNEL2"
-		elif (row['MOVIE'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 0): return "MOVIE"
-		elif (row['MOVIE'] == 0) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "ORIGINALS"
-		elif (row['MOVIE'] == 0) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "ORIGINALS"
+# def getCustomerType(row):
+# 	if row['sum'] == 4: return "DIVERSE"
+# 	elif row['sum'] == 3:
+# 		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 1): return "CHANNEL2_ORIGINAL"
+# 		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "DIVERSE"
+# 		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "DIVERSE"
+# 		elif (row['CHANNEL2'] == 0) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 1): return "ORIGINAL_MOVIES"
+# 	elif row['sum'] == 2:
+# 		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 0): return "CHANNEL2_MOVIE"
+# 		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "CHANNEL2_ORIGINAL"
+# 		elif (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "CHANNEL2_ORIGINAL"
+# 		elif (row['CHANNEL2'] == 0) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "ORIGINAL_MOVIES"
+# 		elif (row['CHANNEL2'] == 0) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "ORIGINAL_MOVIES"
+# 		else: return "ORIGINALS"
+# 	elif row['sum'] == 1:
+# 		if (row['CHANNEL2'] == 1) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 0): return "CHANNEL2"
+# 		elif (row['MOVIE'] == 1) & (row['MOVIE'] == 1) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 0): return "MOVIE"
+# 		elif (row['MOVIE'] == 0) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 1) & (row['ORIGMOVIE'] == 0): return "ORIGINALS"
+# 		elif (row['MOVIE'] == 0) & (row['MOVIE'] == 0) & (row['ORIGSHOW'] == 0) & (row['ORIGMOVIE'] == 1): return "ORIGINALS"
+# 	elif row["sum"] == 0: return "OTHER"
 
-print('getting customer type')
-contenttype = []
-for i in range(len(label)):
-	contenttype.append(getCustomerType(label.iloc[i]))
+# print('getting customer type')
+# contenttype = []
+# for i in range(len(label)):
+# 	contenttype.append(getCustomerType(label.iloc[i]))
 
-label['CONTENT_TYPE_WATCHED'] = contenttype
-print('done')
-label.to_csv('results/customer_feature_matrix_v2.csv', index = False)
-print(label.CONTENT_TYPE_WATCHED.unique())
-print(len(label.loc[label.CONTENT_TYPE_WATCHED] == np.nan))
+# label['CONTENT_TYPE_WATCHED'] = contenttype
+# print('done')
+# label.to_csv('results/customer_feature_matrix_v2.csv', index = False)
+
+label = pd.read_csv('results/customer_feature_matrix_v2.csv')
+tot = len(label)
+for i in label.CONTENT_TYPE_WATCHED.unique():
+	temp = label.loc[label.CONTENT_TYPE_WATCHED == i]
+	print(i, (len(temp)/tot)*100)
--- content/dailycount.py
+++ content/dailycount.py
@@ -14,6 +14,7 @@ from utils import readChunk
 # mpl.use('tKagg')
 from matplotlib import pyplot as plt
 import seaborn as sns
+import matplotlib.ticker as ticker
 
 import matplotlib.style as style
 
@@ -21,11 +22,11 @@ sns.set()
 style.use('seaborn-poster')
 style.use('bmh')
 
-file = "../data/reg_current.csv"
+file = "../data/reg_origshow.csv"
 df = readChunk(file, sep = '\t')
 df.rename(columns = {"DATE(MODIFIEDDATE)":'DATE', "DAYOFWEEK(MIN(MODIFIEDDATE))":'DAYOFWEEK'}, inplace = True)
 
-file3 = "../data/reg_old.csv"
+file3 = "../data/reg_origmovie.csv"
 df3 = readChunk(file3, sep = '\t')
 df3.rename(columns = {"DATE(MODIFIEDDATE)":'DATE', "DAYOFWEEK(MIN(MODIFIEDDATE))":'DAYOFWEEK'}, inplace = True)
 
@@ -43,18 +44,36 @@ label = label.loc[label.LABEL == 'ACTIVE']
 df = df.merge(label, how = 'left', on = 'USERID')
 print(len(df))
 
-new_df = df.group_by('DATE')['USERID'].count().to_frame()
+new_df = df.groupby(['DATE', 'DAYOFWEEK'])['USERID'].count().to_frame()
 print(new_df.head())
 new_df.reset_index(inplace = True)
 
 new_df['DATE'] = pd.to_datetime(new_df['DATE'])
-new_df['DATE'] = new_df['DATE'].dt.date
+# new_df['DATE'] = new_df['DATE'].dt.date
 
+
+def line_format(label):
+	month = label.strftime("%b")[:3]
+	day = label.day
+
+	return month+str(day)
+
+new_df['MONTH'] = new_df.DATE.apply(lambda x: x.strftime("%b")[:3])
 new_df.sort_values('DATE', inplace = True)
 new_df.set_index('DATE', inplace = True)
 
-plot = new_df.plot(kind = 'bar', legend = False)
-plot.set_xlabel('DATE')
-plot.set_ylabel('NUMBER OF CUSTOMERS')
+for i in new_df.MONTH.unique():
+	temp = new_df.loc[new_df.MONTH == i]
+	xcoords = temp.loc[(temp.DAYOFWEEK == '1') | (temp.DAYOFWEEK == '7')].index
+	print(xcoords)
+	temp = temp[['USERID']]
+	plot = temp.plot(kind = 'bar', legend = False)
+	plot.set_xlabel('DATE')
+	plot.set_ylabel('NUMBER OF CUSTOMERS')
+	plot.set_xticklabels(map(lambda x: line_format(x), temp.index))
+	plot.set_ylim(0,90000)
+	for xc in xcoords:
+		plt.axvline(x = xc, color = 'red', linestyle = '--')
+	plt.show()
 
-plt.show()
+
