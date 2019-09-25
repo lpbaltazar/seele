d30f9ce609c66ac9a8102b6d83820a517733c22a
ririgi <lrpbaltazar@gmail.com>
2019-09-25 09:27:46 +0800

    subplots

--- content/dailycount.py
+++ content/dailycount.py
@@ -22,58 +22,60 @@ sns.set()
 style.use('seaborn-poster')
 style.use('bmh')
 
-file = "../data/reg_origshow.csv"
-df = readChunk(file, sep = '\t')
-df.rename(columns = {"DATE(MODIFIEDDATE)":'DATE', "DAYOFWEEK(MIN(MODIFIEDDATE))":'DAYOFWEEK'}, inplace = True)
+def getFile(file1, file2 = None):
+	df = readChunk(file1, sep = '\t')
+	df.rename(columns = {"DATE(MODIFIEDDATE)":'DATE', "DAYOFWEEK(MIN(MODIFIEDDATE))":'DAYOFWEEK'}, inplace = True)
 
-file3 = "../data/reg_origmovie.csv"
-df3 = readChunk(file3, sep = '\t')
-df3.rename(columns = {"DATE(MODIFIEDDATE)":'DATE', "DAYOFWEEK(MIN(MODIFIEDDATE))":'DAYOFWEEK'}, inplace = True)
+	if file2:
+		df2 = readChunk(file2, sep = '\t')
+		df3.rename(columns = {"DATE(MODIFIEDDATE)":'DATE', "DAYOFWEEK(MIN(MODIFIEDDATE))":'DAYOFWEEK'}, inplace = True)
 
-df = pd.concat([df, df3])
-print(df.head())
+		df = pd.concat([df, df2])
+	
+	print(df.head())
+	df.drop_duplicates(subset = ['USERID', 'DATE'], inplace = True)
 
-df.drop_duplicates(subset = ['USERID', 'DATE'], inplace = True)
+	label = pd.read_csv("../data/customer_feature_matrix.csv", usecols = ["userid", "label"])
+	label.columns = label.columns.str.upper()
 
-label = pd.read_csv("../data/customer_feature_matrix.csv", usecols = ["userid", "label"])
-label.columns = label.columns.str.upper()
+	label = label.loc[label.LABEL == 'ACTIVE']
+	df = df.merge(label, how = 'left', on = 'USERID')
+	return df
 
-print(len(df))
+def getNumberofCustomers(df):
+	new_df = df.groupby(['DATE', 'DAYOFWEEK'])['USERID'].count().to_frame()
+	print(new_df.head())
+	new_df.reset_index(inplace = True)
 
-label = label.loc[label.LABEL == 'ACTIVE']
-df = df.merge(label, how = 'left', on = 'USERID')
-print(len(df))
+	new_df['DATE'] = pd.to_datetime(new_df['DATE'])
 
-new_df = df.groupby(['DATE', 'DAYOFWEEK'])['USERID'].count().to_frame()
-print(new_df.head())
-new_df.reset_index(inplace = True)
+	new_df['MONTH'] = new_df.DATE.apply(lambda x: x.strftime("%b")[:3])
+	new_df.sort_values('DATE', inplace = True)
+	new_df.set_index('DATE', inplace = True)
 
-new_df['DATE'] = pd.to_datetime(new_df['DATE'])
-# new_df['DATE'] = new_df['DATE'].dt.date
-
-
-def line_format(label):
-	month = label.strftime("%b")[:3]
-	day = label.day
-
-	return month+str(day)
-
-new_df['MONTH'] = new_df.DATE.apply(lambda x: x.strftime("%b")[:3])
-new_df.sort_values('DATE', inplace = True)
-new_df.set_index('DATE', inplace = True)
-
-for i in new_df.MONTH.unique():
-	temp = new_df.loc[new_df.MONTH == i]
-	xcoords = temp.loc[(temp.DAYOFWEEK == '1') | (temp.DAYOFWEEK == '7')].index
-	print(xcoords)
-	temp = temp[['USERID']]
-	plot = temp.plot(kind = 'bar', legend = False)
-	plot.set_xlabel('DATE')
-	plot.set_ylabel('NUMBER OF CUSTOMERS')
-	plot.set_xticklabels(map(lambda x: line_format(x), temp.index))
-	plot.set_ylim(0,90000)
-	for xc in xcoords:
-		plt.axvline(x = xc, color = 'red', linestyle = '--')
+def plot(new_df, ylim = None):
+	fig, axes = plt.subplots(2,4)
+	x = 0
+	y = 0
+	for i in new_df.MONTH.unique():
+		temp = new_df.loc[new_df.MONTH == i]
+		xcoords = temp.loc[(temp.DAYOFWEEK == '1') | (temp.DAYOFWEEK == '7')].index
+		print(xcoords)
+		temp = temp[['USERID']]
+		axes[x, y] = temp.plot(kind = 'bar', legend = False)
+		axes[x, y].set_xlabel('DATE')
+		axes[x, y].set_ylabel('NUMBER OF CUSTOMERS')
+		axes[x, y].set_xticklabels(map(lambda z: line_format(z), temp.index))
+		if ylim:
+			axes[x, y].set_ylim(0,90000)
+		y = y + 1
+		if y == 4:
+			y = 0
+			x = x + 1
 	plt.show()
 
+if __name__ == '__main__':
+	df = getFile("../data/reg_origshow.csv", "../data/reg_origmovie.csv")
+	df = getNumberofCustomers(df)
+	plot(df, 90000)
 
