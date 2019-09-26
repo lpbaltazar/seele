93064c71c1ba64e99bc2c0673b1303f79fab0b61
ririgi <lrpbaltazar@gmail.com>
2019-09-26 11:07:45 +0800

    get regularity per customer

---
+++ regularity/status/weekly_regularity.py
@@ -0,0 +1,43 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys
+sys.path.append("../")
+
+import os
+import csv
+import time
+import pandas as pd
+import numpy as np
+
+from utils import readChunk, toCSV
+
+file = sys.argv[1]
+outfile = sys.argv[2]
+df = readChunk(file, header = None)
+df.rename(columns = {0:'USERID', 1:'SESSIONID', 2:'MONTH', 3:'WEEK', 4:'DATE', 5:'DAY_OF_WEEK'}, inplace = True)
+print(len(df))
+
+df.WEEK = df.WEEK.astype(int)
+df.DAY_OF_WEEK = df.DAY_OF_WEEK.astype(int)
+binary = []
+count = 0
+with open(outfile, "a") as f:
+	writer = csv.writer(f, delimiter = ',')
+	for i in df.USERID.unique():
+		print(count,'/', len(df.USERID.unique()))
+		temp = df.loc[df.USERID == i]
+		bin_df = pd.DataFrame(index = list(temp.WEEK.unique()), data = 0, columns = list(range(1,8))) 
+		bin_df.index.name = 'WEEK'
+		for j in temp.WEEK.unique():
+			temp2 = temp.loc[temp.WEEK == j]
+			for k in range(len(temp2)):
+				bin_df.loc[int(j)][int(temp2.iloc[k]['DAY_OF_WEEK'])] = 1
+
+		bin_df['RWEEK'] = bin_df[1]+bin_df[2]+bin_df[3]+bin_df[4]+bin_df[5]+bin_df[6]+bin_df[7]	
+		bin_df['USERID'] = i
+		bin_df.reset_index(inplace = True)
+		print(bin_df)
+		count = count+1
+		for j in range(len(bin_df)):
+			writer.writerow(bin_df.iloc[j][:])
