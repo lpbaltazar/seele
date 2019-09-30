c90dd8456bce1a20a19c38b4f101449f9bf966c1
ririgi <lrpbaltazar@gmail.com>
2019-09-30 09:42:24 +0800

    add inplace argument in drop duplicates

--- data/inception_week.py
+++ data/inception_week.py
@@ -18,7 +18,7 @@ print(df.head())
 df.WEEK = df.WEEK.astype(int)
 df.sort_values('WEEK', inplace = True)
 print(len(df))
-df.drop_duplicates(subset = ['USERID'], keep = 'first')
+df.drop_duplicates(subset = ['USERID'], keep = 'first', inplace = True)
 print(len(df))
 
 toCSV(df[['USERID', 'WEEK']], "inception_week.csv", index = False)
