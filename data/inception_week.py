99e6348fe75d2ea8f97feb662d50001dbd1e0e80
ririgi <lrpbaltazar@gmail.com>
2019-09-30 09:39:04 +0800

    add code for getting inception week

---
+++ data/inception_week.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys
+sys.path.append("../")
+
+import os
+import time
+import pandas as pd
+import numpy as np
+
+from utils import readChunk, toCSV
+
+file = "../data/year_week.csv"
+df = readChunk(file, sep = "\t")
+
+print(df.head())
+df.WEEK = df.WEEK.astype(int)
+df.sort_values('WEEK', inplace = True)
+print(len(df))
+df.drop_duplicates(subset = ['USERID'], keep = 'first')
+print(len(df))
+
+toCSV(df[['USERID', 'WEEK']], "inception_week.csv", index = False)
+
+
