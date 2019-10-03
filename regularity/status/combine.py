686745d9c2842eced010279c5451941478104317
ririgi <lrpbaltazar@gmail.com>
2019-10-03 09:31:58 +0800

    edit code to combine monthly

--- regularity/status/combine.py
+++ regularity/status/combine.py
@@ -9,12 +9,12 @@ import time
 import pandas as pd
 import numpy as np
 
-data_dir = "results/regularity"
+data_dir = "results/monthly"
 dfs = []
-for f in sorted(os.listdir(data_dir):
+for f in sorted(os.listdir(data_dir)):
 	if f.endswith(".csv"):
 		dfs.append(pd.read_csv(os.path.join(data_dir, f), header = None))
 		
 dfs = pd.concat(dfs)
-dfs.rename(columns = {0:'WEEK', 8:'RWEEK', 9:'USERID'}, inplace = True)
-dfs.to_csv("results/regularity_combined.csv")
+dfs.rename(columns = {0:'MONTH', 32:'RMONTH', 33:'USERID'}, inplace = True)
+dfs.to_csv("results/regularity_combined_monthly.csv")
